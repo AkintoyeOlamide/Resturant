8353cd5d1d8eda97aa6e868422a0282fcd45d5ae
AkintoyeOlamide <akintoyeolamide500@gmail.com>
2021-04-30 14:17:55 +0100

    fixing linters errors

--- src/about.js
+++ src/about.js
@@ -1,16 +1,16 @@
 const about = () => {
-    const item = document.querySelector('.item');
+  const item = document.querySelector('.item');
 
-    const aboutTitle = document.createElement('h2');
-    aboutTitle.classList.add('center');
-    aboutTitle.textContent = 'Mel-Tea Italian cusine';
+  const aboutTitle = document.createElement('h2');
+  aboutTitle.classList.add('center');
+  aboutTitle.textContent = 'Mel-Tea Italian cusine';
 
-    const aboutText = document.createElement('p');
-    aboutText.classList.add('center');
-    aboutText.textContent = 'Mel Tea Room Ltd is the perfect place for mouthwatering american. We provide a high quality food made to perfection, try our chicken for yourself. Come in and experience our unmatched service and food. Browse our restaurant menu today and give us a shout at (506) 536-1251, we are located in Sackville!.';
+  const aboutText = document.createElement('p');
+  aboutText.classList.add('center');
+  aboutText.textContent = 'Mel Tea Room Ltd is the perfect place for mouthwatering american. We provide a high quality food made to perfection, try our chicken for yourself. Come in and experience our unmatched service and food. Browse our restaurant menu today and give us a shout at (506) 536-1251, we are located in Sackville!.';
 
-    item.appendChild(aboutTitle);
-    item.appendChild(aboutText);
+  item.appendChild(aboutTitle);
+  item.appendChild(aboutText);
 };
 
 export default about;
--- src/contact.js
+++ src/contact.js
@@ -1,41 +1,40 @@
 const createIcon = (class1, class2) => {
-    const link = document.createElement('a');
-    link.classList.add('m-left-10');
-    link.href = '#';
-    const icon = document.createElement('i');
-    icon.classList.add(class1, class2);
-    link.appendChild(icon);
+  const link = document.createElement('a');
+  link.classList.add('m-left-10');
+  link.href = '#';
+  const icon = document.createElement('i');
+  icon.classList.add(class1, class2);
+  link.appendChild(icon);
 
-    return link;
+  return link;
 };
 
 const contact = () => {
-    const item = document.querySelector('.item');
+  const item = document.querySelector('.item');
 
-    const links = document.createElement('h2');
-    links.classList.add('center');
-    links.textContent = '';
+  const links = document.createElement('h2');
+  links.classList.add('center');
+  links.textContent = '';
 
-    const instaLink = createIcon('fab', 'fa-instagram');
-    const twitterLink = createIcon('fab', 'fa-twitter');
-    const fbLink = createIcon('fab', 'fa-facebook-f');
+  const instaLink = createIcon('fab', 'fa-instagram');
+  const twitterLink = createIcon('fab', 'fa-twitter');
+  const fbLink = createIcon('fab', 'fa-facebook-f');
 
-    links.appendChild(instaLink);
-    links.appendChild(twitterLink);
-    links.appendChild(fbLink);
+  links.appendChild(instaLink);
+  links.appendChild(twitterLink);
+  links.appendChild(fbLink);
 
-    const aboutText = document.createElement('p');
-    aboutText.classList.add('contact-text1');
-    aboutText.textContent = "You can contact us here; +44 2456357733";
+  const aboutText = document.createElement('p');
+  aboutText.classList.add('contact-text1');
+  aboutText.textContent = 'You can contact us here; +44 2456357733';
 
-    const contactText = document.createElement('h6');
-    contactText.classList.add('contact-text2');
-    contactText.textContent = "Our UK Adress: plot 10A Totehham Road No4";
+  const contactText = document.createElement('h6');
+  contactText.classList.add('contact-text2');
+  contactText.textContent = 'Our UK Adress: plot 10A Totehham Road No4';
 
-
-    item.appendChild(aboutText);
-    item.appendChild(contactText);
-    item.appendChild(links);
+  item.appendChild(aboutText);
+  item.appendChild(contactText);
+  item.appendChild(links);
 };
 
 export default contact;
